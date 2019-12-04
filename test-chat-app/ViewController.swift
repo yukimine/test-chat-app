90a2677ccb41b0f2b2396bbb948dcd7f427d1574
yukimine <yukimine_hayashi@voyagegroup.com>
2019-12-04 13:19:05 +0900

    Initial Commit

---
+++ test-chat-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-chat-app
+//
+//  Created by yukimine on 2019/12/04.
+//  Copyright © 2019 yukimine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test-chat-appTests/test_chat_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_chat_appTests.swift
+//  test-chat-appTests
+//
+//  Created by yukimine on 2019/12/04.
+//  Copyright © 2019 yukimine. All rights reserved.
+//
+
+import XCTest
+@testable import test_chat_app
+
+class test_chat_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
