49a56cc6cee165b12b9ae60f96af346fb0a2fa3c
yukimine <yukimine_hayashi@voyagegroup.com>
2019-12-06 13:36:18 +0900

    UIを追加

--- test-chat-app/ViewController.swift
+++ test-chat-app/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var message: [String] = []
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bodyTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return message.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+
+        cell.textLabel!.text = message[indexPath.row]
+        
+        return cell
+    }
+
+    @IBAction func touchedSubmitButton(_ sender: UIButton) {
+        guard let body = bodyTextField.text else { return }
+
+        message.append(body)
+        tableView.reloadData()
+    }
 }
-
